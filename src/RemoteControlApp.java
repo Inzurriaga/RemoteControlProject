7c6a9dae3a0279a9f5671c20e56dccb92dabe1ca
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-11-08 16:43:05 -0700

    Added TODO's

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -2,8 +2,8 @@
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO: add press button method 
+		// TODO: add turn on button
 	}
 
 }
