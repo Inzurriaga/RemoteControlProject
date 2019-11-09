65119986fa183f32c093b4a87207fda9d0bb5578
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-11-08 17:02:15 -0700

    Complete Java Code for Remote Control App

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO: add press button method 
-		// TODO: add turn on button
+		Scanner input = new Scanner(System.in);
+		String response;
+		do {
+			System.out.print("To begin, turn on the remote control:");
+			response = input.next();
+		} while (!response.equals("ON"));
+		System.out.println("TURNING ON...");
+		while (!response.equals("OFF")) {
+			response = input.next();
+			switch (response) {
+				case "0":
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+					pressButton();
+					break;
+				case "OFF":
+					break;
+				default:
+					System.out.println("Command not recognized");
+			}
+		}
+		input.close();
+		System.out.println("TURNING OFF...");
 	}
 
+	public static void pressButton() {
+		System.out.println("BOOP!");
+	}
 }
